da1acb4fc73ff322f708714f0862bf055dc6f2cb
adelmo aquino <80642826+aquinoartur@users.noreply.github.com>
2024-03-18 09:12:59 -0300

    Initial Commit

---
+++ PrimeiroAppTableViewCurso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeiroAppTableViewCurso
+//
+//  Created by Adelmo Artur de Aquino on 18/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
